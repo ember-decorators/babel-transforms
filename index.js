06ebf1262f415d97385313261693b764b44cc0a4
Jan Buschtöns <buschtoens@gmail.com>
2017-12-14 20:53:14 +0100

    refactor: extract babel setup helper into own module (#5)
    * fix(build): only call _super.included, if it is set
    
    * Revert "fix(build): only call _super.included, if it is set"
    
    This reverts commit e9f65b696566658e0e6e891c2b099f9345a326ac.
    
    * refactor: extract babel setup helper into own module
    
    This allows us to explicitly call it from other addons, like in `ember-decorators`, where we need to do that for backwards compatibility reasons.
    
    * docs: fix typo 'nox' -> 'not'
    
    * refactor: move setup-babel to private directory

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
 /* eslint-env node */
 'use strict';
 
-const path = require('path');
-const VersionChecker = require('ember-cli-version-checker');
-
-function requireTransform(transformName) {
-  let plugin = require(transformName);
-
-  plugin = plugin.__esModule ? plugin.default : plugin;
-
-  // adding `baseDir` ensures that broccoli-babel-transpiler does not
-  // issue a warning and opt out of caching
-  const pluginPath = require.resolve(`${transformName}/package`);
-  const pluginBaseDir = path.dirname(pluginPath);
-  plugin.baseDir = () => pluginBaseDir;
-
-  return plugin;
-}
-
-function hasPlugin(plugins, name) {
-  for (const maybePlugin of plugins) {
-    const plugin = Array.isArray(maybePlugin) ? maybePlugin[0] : maybePlugin;
-    const pluginName = typeof plugin === 'string' ? plugin : plugin.name;
-
-    if (pluginName === name) {
-      return true;
-    }
-  }
-
-  return false;
-}
+const setupBabel = require('./private/setup-babel');
 
 module.exports = {
   name: '@babel-decorators/babel-transforms',
@@ -37,7 +9,7 @@ module.exports = {
   included(parent) {
     this._super.included.apply(this, arguments);
 
-    // Create parent options, if they do nox exist
+    // Create parent options, if they do not exist
     const parentOptions = (parent.options = parent.options || {});
 
     // Create our own options, if they do not exist
@@ -45,40 +17,7 @@ module.exports = {
       parentOptions['@ember-decorators/babel-transforms'] || {});
 
     if (!this._registeredWithBabel && !ownOptions.disable) {
-      const checker = new VersionChecker(this.parent).for(
-        'ember-cli-babel',
-        'npm'
-      );
-
-      if (checker.satisfies('^6.0.0-beta.1')) {
-        const TransformDecoratorsLegacy = requireTransform(
-          'babel-plugin-transform-decorators-legacy'
-        );
-        const TransformClassProperties = requireTransform(
-          'babel-plugin-transform-class-properties'
-        );
-
-        // Create babel options if they do not exist
-        parentOptions.babel = parentOptions.babel || {};
-
-        // Create and pull off babel plugins
-        const plugins = (parentOptions.babel.plugins =
-          parentOptions.babel.plugins || []);
-
-        if (!hasPlugin(plugins, 'transform-decorators-legacy')) {
-          // unshift the transform because it always must come before class properties
-          plugins.unshift(TransformDecoratorsLegacy);
-        }
-
-        if (!hasPlugin('transform-class-properties')) {
-          plugins.push(TransformClassProperties);
-        }
-      } else {
-        parent.project.ui.writeWarnLine(
-          '@ember-decorators/babel-transforms: You are using an unsupported ember-cli-babel version, decorator/class-property transforms will not be included automatically'
-        );
-      }
-
+      setupBabel(parent);
       this._registeredWithBabel = true;
     }
   }
